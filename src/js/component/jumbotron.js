81018cbfd09d9bb67f6a3522645ee941344f24c4
camillawilliams <camillawilliamsdu@gmail.com>
2021-02-16 01:07:10 +0000

    added new jumbo

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,30 +1,19 @@
 import React, { Component } from "react";
 // import image5 from "../../img/image5.jpg";
 
-export const Jumbotron = () => (
-  
-        <section className="clean-block clean-hero" style="background-image:url(&quot;/assets/img/tech/image4.jpg?h=0adc0de9a17a7caea6b8641f5d6ae02c&quot;);color:rgba(9, 162, 255, 0.85);">
-            <div className="text">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p><a className="btn btn-outline-light active btn-lg" role="button" href="/forecast.html">Forecast Investments</a>
-            </div>
-        </section>
-        <section className="clean-block clean-info dark">
-            <div className="container">
-                <div className="block-heading">
-                    <h2 className="text-info">Info</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <div className="row align-items-center">
-                    <div className="col-md-6"><img className="img-thumbnail" src="/assets/img/scenery/image5.jpg?h=5a16d46fccd884924ce66752802d76c5" /></div>
-                    <div className="col-md-6">
-                        <h3>Lorem impsum dolor sit amet</h3>
-                        <div className="getting-started-info">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div><button className="btn btn-outline-primary btn-lg" type="button">Join Now</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-     
-);
+export const Jumbotron = () => {
+	return (
+		<>
+			<Jumbotron>
+				<h1>Hello, world!</h1>
+				<p>
+					This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+					featured content or information.
+				</p>
+				<p>
+					<Button variant="primary">Learn more</Button>
+				</p>
+			</Jumbotron>
+		</>
+	);
+};
