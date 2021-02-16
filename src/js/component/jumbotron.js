2c6d9f2669809e7c8c7b19d14628c9d4102163d0
amafjarkasi <amafjarkasi@gmail.com>
2021-02-16 00:44:38 +0000

    latest updates

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import image5 from "../../img/image5.jpg";
 
 export const Jumbotron = () => (
-    <div>
+  
         <section className="clean-block clean-hero" style="background-image:url(&quot;/assets/img/tech/image4.jpg?h=0adc0de9a17a7caea6b8641f5d6ae02c&quot;);color:rgba(9, 162, 255, 0.85);">
             <div className="text">
                 <h2>Lorem ipsum dolor sit amet.</h2>
@@ -16,7 +16,7 @@ export const Jumbotron = () => (
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
                 </div>
                 <div className="row align-items-center">
-                    <div className="col-md-6"><img className="img-thumbnail" src="/assets/img/scenery/image5.jpg?h=5a16d46fccd884924ce66752802d76c5"></div>
+                    <div className="col-md-6"><img className="img-thumbnail" src="/assets/img/scenery/image5.jpg?h=5a16d46fccd884924ce66752802d76c5" /></div>
                     <div className="col-md-6">
                         <h3>Lorem impsum dolor sit amet</h3>
                         <div className="getting-started-info">
@@ -26,5 +26,5 @@ export const Jumbotron = () => (
                 </div>
             </div>
         </section>
-        </div>
+     
 );
